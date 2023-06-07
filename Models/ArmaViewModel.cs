63082764c406bb27aa2d764453a57de520075009
Breno-Lisboa-Souza <brenolisboa2004@gmail.com>
2023-06-06 22:18:45 -0300

    Atribuição de Armas a personagens existentes

---
+++ Models/ArmaViewModel.cs
@@ -0,0 +1,14 @@
+using RpgMvc.Models.Enuns;
+
+
+namespace RpgMvc.Models
+{
+    public class ArmaViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Dano { get; set; }
+        //public Personagem Personagem { get; set; }
+        public int PersonagemId { get; set; }
+    }
+}
